91d8b4e90e20a7ec9b9bfed2907b06a6ce34512b
Leonid Korokh <lkorokh@outlook.com>
2018-03-20 00:29:34 +0300

    add create db migration

---
+++ src/ACME.MicrosoftCA.Gateway/Migrations/20180319210921_000_CreateDB.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ACME.MicrosoftCA.Gateway.Migrations
+{
+    public partial class _000_CreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IssuedNonces",
+                columns: table => new
+                {
+                    Nonce = table.Column<string>(nullable: false),
+                    IssedAt = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IssuedNonces", x => x.Nonce);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IssuedNonces");
+        }
+    }
+}
